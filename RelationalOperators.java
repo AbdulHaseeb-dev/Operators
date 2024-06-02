4dd3cd518bb7131437cceda5e0a3fb9adca1534b
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-06-02 21:32:45 +0500

    Learned about different Relational Operators used in Java

---
+++ RelationalOperators.java
@@ -0,0 +1,14 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 5;
+        System.out.println(a == b);
+        System.out.println(b == c);
+        System.out.println(b != c);
+        System.out.println(b > c);
+        System.out.println(a < c);
+        System.out.println(a >= b);
+        System.out.println(b <= c);
+    }
+}
