bd910b86e051f7235b9613734c0b82a0f11c9a3f
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-06-02 21:24:14 +0500

    Learned about pre increment, post increment, pre decrement and post decrement

---
+++ UnaryOperators.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class UnaryOperators {
+    public static void main(String[] args) {
+        
+    //   pre increment
+
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter A");
+    // int a = sc.nextInt();
+    // int b = ++a;
+    // System.out.println(a);
+    // System.out.println(b);
+
+    // sc.close();
+
+    // post decrement
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter C");
+    int c = sc.nextInt();
+    int d = c++;
+    System.out.println(c);
+    System.out.println(d);
+
+    sc.close();
+
+    }
+}
