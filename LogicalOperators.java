cae8ed14a12ee29ec767353bf3070958a7423119
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-06-03 20:44:18 +0500

    Learned about logical AND, OR and NOT

---
+++ LogicalOperators.java
@@ -0,0 +1,14 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+
+        // Logical AND (&&)
+        System.out.println((3>2) && (5>3));
+
+        // Logical OR (||)
+        System.out.println((3<2) || (5>3));
+
+        // Logical NOT (!)
+        System.out.println(!(3>2));
+        System.out.println(!(5<8));
+    }
+}
