157f0e8471ac6c6d4eae9bef3d61950cc4f58642
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-06-03 21:06:31 +0500

    Learned about assignment operators

---
+++ AssignmentOperators.java
@@ -0,0 +1,25 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        // a = a + 10;
+        a += 10;
+        int b = 15;
+        // b = b - 3;
+        b -= 3;
+        int c = 5;
+        // c = c * 5;
+        c *= 5;
+        int d = 6;
+        // d = d / 2;
+        d /= 2;
+        int e = 15;
+        // e = e % 3;
+        e %= 3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
